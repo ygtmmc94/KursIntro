57a94c32bc96327ab6f40f90d6e2c85e0c9fb9ff
Yiğit MMC <yigit@horda.games>
2022-08-29 17:38:47 +0300

    GamerManager

--- OOP2/TuzelMusteri.cs
+++ OOP2/TuzelMusteri.cs
@@ -5,11 +5,11 @@ using System.Text;
 namespace OOP2
 {
     // Coorporate
-    class TuzelMusteri:Musteri // inheritance
+    class TuzelMusteri : Musteri // inheritance
     {
-        
+
         public string SirketAdi { get; set; }
         public string VergiNo { get; set; }
-        
+
     }
 }
